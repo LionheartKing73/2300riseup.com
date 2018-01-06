885ba4b9d34b652e0866a6b8ba8e378f8784d492
webstar1987923 <webstar1987923@hotmail.com>
2018-01-06 19:34:57 +0300

    done

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+
+		<title>2300 Mt Olympus Dr, Los Angeles, CA 90046</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+
+		<link rel="stylesheet" href="css/layout.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="css/supersized.css" type="text/css" media="screen" />
+		<link rel="stylesheet" href="theme/supersized.shutter.css" type="text/css" media="screen" />
+		<link href='http://fonts.googleapis.com/css?family=Playfair+Display+SC' rel='stylesheet' type='text/css'>
+
+		<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+
+
+
+ 
+
+        <script src="js/redirection-mobile.js"></script>
+            <script>
+
+			  SA.redirection_mobile ({
+				  tablet_redirection : "true",
+				  mobile_url : "",
+				  mobile_prefix : "http"
+			  });
+
+            </script>
+            
+		<script type="text/javascript" src="js/supersized.3.2.7.min.js"></script>
+		<script type="text/javascript" src="theme/supersized.shutter.min.js"></script>            
+		<script type="text/javascript">
+
+			jQuery(function($){
+
+				$.supersized({
+
+					// Functionality
+					slide_interval   : 4500,  // Length between transitions
+					transition       :    1,  // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+					transition_speed : 1500,  // Speed of transition
+
+					// Components
+					slide_links      : false, // Individual links for each slide (Options: false, 'num', 'name', 'blank')
+					slides           :          // Slideshow Images
+                [
+{image : '/galleryImages/001.jpg', title : ' ', thumb : 'galleryImages/thumbnail/001.jpg'},
+{image : '/galleryImages/002.jpg', title : ' ', thumb : 'galleryImages/thumbnail/002.jpg'},
+{image : '/galleryImages/003.jpg', title : ' ', thumb : 'galleryImages/thumbnail/003.jpg'},
+{image : '/galleryImages/004.jpg', title : ' ', thumb : 'galleryImages/thumbnail/004.jpg'},
+{image : '/galleryImages/005.jpg', title : ' ', thumb : 'galleryImages/thumbnail/005.jpg'},
+{image : '/galleryImages/006.jpg', title : ' ', thumb : 'galleryImages/thumbnail/006.jpg'},
+{image : '/galleryImages/007.jpg', title : ' ', thumb : 'galleryImages/thumbnail/007.jpg'},
+{image : '/galleryImages/008.jpg', title : ' ', thumb : 'galleryImages/thumbnail/008.jpg'},
+{image : '/galleryImages/009.jpg', title : ' ', thumb : 'galleryImages/thumbnail/009.jpg'},
+{image : '/galleryImages/010.jpg', title : ' ', thumb : 'galleryImages/thumbnail/010.jpg'},
+{image : '/galleryImages/011.jpg', title : ' ', thumb : 'galleryImages/thumbnail/011.jpg'},
+{image : '/galleryImages/012.jpg', title : ' ', thumb : 'galleryImages/thumbnail/012.jpg'},
+{image : '/galleryImages/013.jpg', title : ' ', thumb : 'galleryImages/thumbnail/013.jpg'},
+{image : '/galleryImages/014.jpg', title : ' ', thumb : 'galleryImages/thumbnail/014.jpg'},
+{image : '/galleryImages/015.jpg', title : ' ', thumb : 'galleryImages/thumbnail/015.jpg'},
+{image : '/galleryImages/016.jpg', title : ' ', thumb : 'galleryImages/thumbnail/016.jpg'},
+{image : '/galleryImages/017.jpg', title : ' ', thumb : 'galleryImages/thumbnail/017.jpg'},
+{image : '/galleryImages/018.jpg', title : ' ', thumb : 'galleryImages/thumbnail/018.jpg'},
+{image : '/galleryImages/019.jpg', title : ' ', thumb : 'galleryImages/thumbnail/019.jpg'},
+{image : '/galleryImages/020.jpg', title : ' ', thumb : 'galleryImages/thumbnail/020.jpg'},
+{image : '/galleryImages/021.jpg', title : ' ', thumb : 'galleryImages/thumbnail/021.jpg'},
+{image : '/galleryImages/022.jpg', title : ' ', thumb : 'galleryImages/thumbnail/022.jpg'},
+{image : '/galleryImages/023.jpg', title : ' ', thumb : 'galleryImages/thumbnail/023.jpg'},
+{image : '/galleryImages/024.jpg', title : ' ', thumb : 'galleryImages/thumbnail/024.jpg'},
+{image : '/galleryImages/025.jpg', title : ' ', thumb : 'galleryImages/thumbnail/025.jpg'},
+{image : '/galleryImages/026.jpg', title : ' ', thumb : 'galleryImages/thumbnail/026.jpg'},
+{image : '/galleryImages/027.jpg', title : ' ', thumb : 'galleryImages/thumbnail/027.jpg'},
+{image : '/galleryImages/028.jpg', title : ' ', thumb : 'galleryImages/thumbnail/028.jpg'}
+                ]
+				});
+		    });
+function show_overlay(overlay){
+  $(overlay).fadeIn('fast');
+}
+		</script>	            
+            
+  <style>
+    html, body {
+      margin: 0;
+      width: 100%;
+      height: 100%;
+    }
+  </style>
+
+  <link rel="stylesheet" href="css/normalize.css" media="all">
+    <link rel="stylesheet" href="css/style.css" media="all">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic|Raleway:400,200,300,500,700,600,800,900' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+    
+    <script src="js/device.min.js"></script> <!--OPTIONAL JQUERY PLUGIN-->
+    <script src="js/jquery.mb.YTPlayer.js"></script>
+    <script src="js/custom.js"></script>          
+
+
+
+</head>
+
+<body>
+<video autoplay="" loop="" style="margin: auto; position: absolute; z-index: -1; top: 50%; left: 50%; transform: translate(-50%, -50%); visibility: visible; width: 100%; height: 100%;"><source src="video/property.mp4" type="video/mp4"></video>
+
+   <!--  <section class="big-background">
+        <a id="bgndVideo" class="player" data-property="{videoURL:'https://www.youtube.com/watch?v=4AkUJvo6DI0&list=PL8cHMR1xOwgbRmEnCRQBYKgq4bsIyI94d',containment:'body',autoPlay:true, mute:true, startAt:0, opacity:1}"></a>
+        
+        <div class="pattern"></div> 
+            <div class="big-background-container">
+                    
+            </div>                                                    
+    </section> -->
+
+<!--Loading display while images load-->
+<div id="loading">&nbsp;</div>
+  <div id="navBar" style="z-index: 10001;">
+  <ul class="right">
+  <li><a href="#" onclick="show_overlay('#popup_content_162');">FEATURES</a></li><li><a href="#" onclick="show_overlay('#popup_content_133');">VIDEO</a></li><li><a href="#" onclick="show_overlay('#popup_floorplan');">FLOORPLAN</a></li><li><a href="#" onclick="show_overlay('#popup_contact');">CONTACT</a></li><li><a href="#" onclick="show_overlay('#popup_gallery');">GALLERY</a></li></ul>
+
+    <ul style="line-height:20px;">
+      <li style="color:#fff; text-align:left; margin-top:20px;">2300 Mt Olympus Dr, Los Angeles, CA 90046<br />
+		4 Bed  | 3.5 Bath | 3,738  Sqft </li>
+    </ul>
+  
+    <ul class="address">
+      <li><img src="img/kw-lrg.png" height="50" alt="Keller Williams"/ style="margin-top:15px;"></li>
+      <li><img src="/img/skor.png" height="60" alt="SKOR Real Estate Group"/ style="margin-top:10px;"></li>
+    </ul>
+  </div> 
+
+<!--Thumbnail Navigation-->
+<div id="prevthumb"></div>
+<div id="nextthumb"></div>
+
+<!--Arrow Navigation-->
+<a id="prevslide" class="load-item"></a>
+<a id="nextslide" class="load-item"></a>
+
+<div id="thumb-tray" class="load-item">
+	<div id="thumb-back"></div>
+	<div id="thumb-forward"></div>
+</div>
+
+
+
+<!--Control Bar-->
+<div id="controls-wrapper" class="load-item">
+	<div id="controls">
+    
+		<a id="play-button"><img id="pauseplay" src="img/pause.png"/></a>
+	
+		<!--Slide counter-->
+		<div id="slidecounter">
+			<span class="slidenumber"></span> / <span class="totalslides"></span>
+		</div>
+		
+		<!--Slide captions displayed here-->
+		<div id="slidecaption"></div>
+		
+		<!--Thumb Tray button-->
+		<a id="tray-button"><img id="tray-arrow" src="img/button-tray-up.png"/></a>
+
+	</div>
+</div>
+
+
+<div class="overlay_container" id="popup_content_162">
+  <div class="simple_overlay">
+    <div class="close" onclick="hide_overlay('#popup_content_162');"></div>
+    <div class="simple_overlay_content">
+<div class="content_block"><h2 style="text-align: center;"><strong>2300 Mt Olympus Dr, Los Angeles, CA 90046</strong></h2>
+<br />
+
+<p style="line-height:1.5em;">Multilevel Modern Architectural in the Hollywood Hills. Sleek and clean finishes characterize this 4 bed | 3.5 bath home with open spaces, high ceilings, LED recessed lighting and grand windows offering natural light and green views. Artistic and dramatic entryway showcase diverging angles and a beautiful staircase for those who appreciate design. Contemporary sunken formal living room with fireplace surrounded by white walls, large windows and two balconies framing the views of the forestry. Gorgeous custom glass chandelier dripping down from 12ft ceilings serving as a centerpiece for the private and elegant dining room. Galley Kitchen featuring Viking Appliances, seamless custom cabinetry and separate breakfast area. Relaxing top floor Master Retreat with stone wall fireplace, balcony and spacious walk in closet. Floating cabinetry, lighted mirrors, double sinks, double rain shower heads and standalone tub with green and lush views of the hills make up the lavish master bathroom.  Remaining sizable bedrooms on third level. Great outdoor living with large sitting areas on newly constructed mahogany deck, grassy backyard with BBQ and Jacuzzi- California lifestyle at its best!</p>
+
+<p>&nbsp;</p>
+
+</div>
+</div></div></div>
+
+
+<div class="overlay_container" id="popup_floorplan">
+  <div class="simple_overlay">
+    <div class="close" onclick="hide_overlay('#popup_floorplan');"></div>
+    <div class="simple_overlay_content">
+      <div class="content_block">
+	    <img src="floorPlan/001.jpg" alt="Floorplan" width="100%"/> 
+        <img src="floorPlan/002.jpg" alt="Floorplan" width="100%"/> 
+        <img src="floorPlan/003.jpg" alt="Floorplan" width="100%"/> 
+      </div>
+</div></div></div>
+
+
+<div class="overlay_container" id="popup_contact">
+  <div class="simple_overlay">
+    <div class="close" onclick="hide_overlay('#popup_contact');"></div>
+    <div class="simple_overlay_content">
+<table width="843">
+
+<tbody>
+
+<tr>
+
+<td style="text-align: center;"><strong style="font-size:1.4em;">Shawn Kormondy</strong><br /><br />
+
+<p style="text-align: center;">Keller Williams Realty<br />439 North Canon Drive, Suite #300<br />Beverly Hills, CA 90210</p>
+
+<p style="text-align: center;">&nbsp;</p>
+
+<p style="text-align: center;">Office: (323) 638-7567</p>
+
+<p style="text-align: center;">&nbsp;</p>
+
+<p style="text-align: center;">info@SkorReg.com<br />www.SkorReg.com<br /><br /></p>
+
+<p style="text-align: center;"><img src="img/Shawn.jpg" width="140" height="140" alt=""/>
+<br /><br /></p>
+
+</td>
+
+<td style="text-align: center;"><strong style="font-size:1.4em;">Katia Miramontes</strong><br /><br />
+
+<p style="text-align: center;">Keller Williams Realty<br />439 North Canon Drive, Suite #300<br />Beverly Hills, CA 90210</p>
+
+<p style="text-align: center;">&nbsp;</p>
+
+<p style="text-align: center;">Office: (323) 638-7567</p>
+
+<p style="text-align: center;">&nbsp;</p>
+
+<p style="text-align: center;">www.kw.com<br />www.realestatekatia.com<br />&nbsp;<br /><br /></p>
+
+<p style="text-align: center;"><img src="img/Katia.jpg" width="140" height="140" alt=""/>
+<br /><br /></p>
+
+</td>
+
+</tr>
+
+</tbody>
+
+</table>
+
+<p>&nbsp;</p>
+</div></div></div>
+
+<div class="overlay_container" id="popup_gallery">
+  <div class="simple_overlay">
+    <div class="close" onclick="hide_overlay('#popup_gallery');"></div>
+    <div class="simple_overlay_content">
+<h2 class="title">Gallery</h2><div class="photos">
+<div class="photo"><img src="/galleryImages/thumbnail/001.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/002.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/003.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/004.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/005.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/006.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/007.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/008.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/009.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/010.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/011.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/012.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/013.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/014.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/015.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/016.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/017.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/018.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/019.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/020.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/021.jpg"></div><!-- <div class="photo"><img src="/galleryImages/thumbnail/022.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/023.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/024.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/025.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/026.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/027.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/028.jpg"></div><div class="photo"> --><?php /*?><img src="/galleryImages/thumbnail/029.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/030.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/031.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/032.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/033.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/034.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/035.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/036.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/037.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/038.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/039.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/040.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/041.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/042.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/043.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/044.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/045.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/046.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/047.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/048.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/049.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/050.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/051.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/052.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/053.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/054.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/055.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/056.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/057.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/058.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/059.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/060.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/061.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/062.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/063.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/064.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/065.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/066.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/067.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/068.jpg"></div><div class="photo"><img src="/galleryImages/thumbnail/069.jpg"></div><?php */?></div>
+</div></div></div>
+
+
+
+
+
+
+  <script type="text/javascript">
+  var current_overlay;
+    function show_overlay(overlay){ hide_overlay(current_overlay); current_overlay = overlay; $(overlay).fadeIn(400); }
+    function hide_overlay(overlay){ $(overlay).fadeOut(400); }
+	show_overlay('#popup_content_133');	
+  </script>
+</body>
+</html>
